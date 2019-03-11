d74ede818bd5056981839491f91442f38795a1a1
Zeuxis Lo <seekstudio@gmail.com>
2019-03-11 17:07:49 +0800

    add precedences for parser

---
+++ parser/precedences.go
@@ -0,0 +1,35 @@
+package parser
+
+import (
+	"github.com/zeuxisoo/go-skriplang/token"
+)
+
+const (
+	_           int = iota
+	LOWEST          //
+	ANDOR           // || or &&
+	EQUALS          // ==
+	LESSGREATER     // > or <
+	SUM             // +
+	PRODUCT         // *
+	PREFIX          // -X or !X
+	CALL            // func(X)
+	INDEX           // array[index]
+)
+
+var precedences = map[token.Type]int{
+	token.EQ: 				EQUALS,
+	token.NOT_EQ: 			EQUALS,
+	token.AND: 				ANDOR,
+	token.OR: 				ANDOR,
+	token.LT: 				LESSGREATER,
+	token.LTEQ:				LESSGREATER,
+	token.GT: 				LESSGREATER,
+	token.GTEQ: 			LESSGREATER,
+	token.PLUS: 			SUM,
+	token.MINUS: 			SUM,
+	token.SLASH: 			PRODUCT,
+	token.ASTERISK: 		PRODUCT,
+	token.LEFT_PARENTHESIS: CALL,
+	token.LEFT_BRACKET: 	INDEX,
+}
