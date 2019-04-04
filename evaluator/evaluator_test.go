73c2bdeb7a47be338cb50a9d53e4f5932d98c865
Zeuxis Lo <seekstudio@gmail.com>
2019-04-04 20:03:13 +0800

    add identifer expression evaluate test case

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -77,6 +77,69 @@ func TestStringLiteralExpression(t *testing.T) {
 	})
 }
 
+func TestIdentifierExpression(t *testing.T) {
+	Convey("Identifier expression test", t, func() {
+		Convey("Error handling test", func() {
+			expecteds := []struct{
+				source string
+				result string
+			}{
+				{ "foo",    "Identifier not found: foo" },
+				{ "foobar", "Identifier not found: foobar" },
+			}
+
+			for index, expected := range expecteds {
+				Convey(runMessage("Running: %d, ", index), func() {
+					evaluated := testEval(expected.source)
+
+					Convey(runMessage("Source: %s", expected.source), func() {
+						testErrorObject(evaluated, expected.result)
+					})
+				})
+			}
+		})
+
+		Convey("Get identifier from environment test", func() {
+			expecteds := []struct{
+				source string	// identifier
+				value  object.Object
+				result interface{}
+			}{
+				{ "foo",	&object.String{ Value: "fooString" },	"fooString" },
+				{ "bar",	&object.Integer{ Value: 5 },			5 },
+			}
+
+			environment := object.NewEnvironment()
+			for _, expected := range expecteds {
+				environment.Set(expected.source, expected.value)
+			}
+
+			for index, expected := range expecteds {
+				Convey(runMessage("Running: %d", index), func() {
+					evaluated := testEvalWithEnv(expected.source, environment)
+
+					Convey(runMessage("Source: %s, expected: %t", expected.source, expected.result), func() {
+						testLiteralObject(evaluated, expected.result)
+					})
+				})
+			}
+		})
+
+		Convey("Register and Get built-in function test", func() {
+			RegisterBuiltIn(
+				"fooFunction",
+				func(environment *object.Environment, arguments ...object.Object) object.Object {
+					return &object.String{
+						Value: "foo function",
+					}
+				},
+			)
+
+			testBuiltInObject(testEval("fooFunction"), "foo function")
+		})
+	})
+}
+
 func TestReturnStatement(t *testing.T) {
 	Convey("Return statement test", t, func() {
 		expecteds := []struct{
@@ -101,25 +164,34 @@ func TestReturnStatement(t *testing.T) {
 
 //
 func testEval(source string) object.Object {
+	return testEvalWithEnv(source, object.NewEnvironment())
+}
+
+func testEvalWithEnv(source string, environment *object.Environment) object.Object {
 	theLexer       := lexer.NewLexer(source)
 	theParser      := parser.NewParser(theLexer)
 	theProgarm 	   := theParser.Parse()
-	theEnvironment := object.NewEnvironment()
 
-	return Eval(theProgarm, theEnvironment)
+	return Eval(theProgarm, environment)
 }
 
-func testDecimalObject(obj object.Object, expected interface{}) {
+func testLiteralObject(obj object.Object, expected interface{}) {
 	switch expected := expected.(type) {
 	case int:
 		testIntegerObject(obj, int64(expected))
 	case int64:
 		testIntegerObject(obj, expected)
+	case string:
+		testStringObject(obj, expected)
 	case float32:
 		testFloatObject(obj, float64(expected))
 	case float64:
 		testFloatObject(obj, expected)
 	}
+}
+
+func testDecimalObject(obj object.Object, expected interface{}) {
+	testLiteralObject(obj, expected)
 }
 
 func testIntegerObject(obj object.Object, expected int64) {
@@ -155,6 +227,28 @@ func testStringObject(obj object.Object, expected string) {
 
 	Convey(runMessage("Object result should be equals %s", expected), func() {
 		So(result.Value, ShouldEqual, expected)
+	})
+}
+
+func testBuiltInObject(obj object.Object, expected string) {
+	result, ok := obj.(*object.BuiltIn)
+
+	Convey("Can convert to object (built-in)", func() {
+		So(ok, ShouldBeTrue)
+	})
+
+	testLiteralObject(result.Function(object.NewEnvironment()), expected)
+}
+
+func testErrorObject(obj object.Object, expected string) {
+	result, ok := obj.(*object.Error)
+
+	Convey("Can convert to object (error)", func() {
+		So(ok, ShouldBeTrue)
+	})
+
+	Convey(runMessage("Object result should be equals %s", expected), func() {
+		So(result.Message, ShouldEqual, expected)
 	})
 }
 
